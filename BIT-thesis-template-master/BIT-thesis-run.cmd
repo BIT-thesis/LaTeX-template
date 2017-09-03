74c2faaf55baebdbf554798d212180ec5201750c
YangYating <yangyating@bit.edu.cn>
2017-09-03 23:29:04 +0800

    格式调整

--- BIT-thesis-template-master/BIT-thesis-run.cmd
+++
@@ -1,9 +0,0 @@
-
-del demo.pdf
-xelatex -no-pdf --interaction=nonstopmode demo
-bibtex demo
-xelatex -no-pdf --interaction=nonstopmode demo
-xelatex --interaction=nonstopmode demo
-
-start "" "demo.pdf"
-
